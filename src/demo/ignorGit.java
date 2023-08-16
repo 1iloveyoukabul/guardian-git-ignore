b2bd0aab2bf0babe1845de86e0c3a8f0173ade0c
babrak <babrak_m@hotmail.com>
2023-08-16 18:03:33 -0400

    initial changes

---
+++ src/demo/ignorGit.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class ignorGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("This is ignoring practice");
+	}
+
+}
